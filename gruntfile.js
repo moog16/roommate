e33e99f55a5f1956f367a1195a3707a3cacef4b2
Matthew Goo <mattgoo@gmail.com>
2013-09-11 22:54:15 -0700

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,95 @@
+module.exports = function(grunt) {
+    // Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            jade: {
+                files: ['app/views/**'],
+                options: {
+                    livereload: true,
+                },
+            },
+            html: {
+                files: ['public/views/**'],
+                options: {
+                    livereload: true,
+                },
+            },
+            js: {
+                files: ['public/js/**'],
+                options: {
+                    livereload: true,
+                },
+            },
+            css: {
+                files: ['public/sass/**'],
+                tasks: ['compass'],
+                options: {
+                    livereload: true,
+                    force: true
+                }
+            }
+        },
+        jshint: {
+             all: ['gruntfile.js', 'public/js/**/*.js', 'test/**/*.js', 'app/**/*.js']
+        },
+        compass: { //Task
+            dist: { //Target
+                options: { //Target options
+                    sassDir: 'public/sass',
+                    cssDir: 'public/css',
+                    environment: 'production'
+                }
+            },
+            dev: { //Another target
+                options: {
+                    sassDir: 'public/sass',
+                    cssDir: 'public/css'
+                }
+            }
+        },
+        nodemon: {
+            dev: {
+                options: {
+                    file: 'server.js',
+                    args: [],
+                    ignoredFiles: ['README.md', 'node_modules/**', '.DS_Store'],
+                    watchedExtensions: ['js'],
+                    watchedFolders: ['app', 'config'],
+                    debug: true,
+                    delayTime: 1,
+                    env: {
+                        PORT: 3000
+                    },
+                    cwd: __dirname
+                }
+            },
+            exec: {
+                options: {
+                    exec: 'less'                    
+                }
+            }
+        },
+        concurrent: {
+            target: {
+                tasks: ['nodemon', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        }
+    });
+
+    //Load NPM tasks 
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    //Making grunt default to force in order not to break the project.
+    grunt.option('force', true);
+
+    //Default task(s).
+    grunt.registerTask('default', ['jshint', 'compass', 'concurrent:target']);
+};
