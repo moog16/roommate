78a359a70d0779f1ec5f3f7f6726d223d475dab8
Matthew Goo <mattgoo@gmail.com>
2013-09-13 15:24:39 -0700

    got signin to work with user update info

--- app/controllers/insert.js
+++ app/controllers/insert.js
@@ -1,8 +1,40 @@
 var mongoose = require('mongoose'),
   User = mongoose.model('User'),
-  api = require('./api.js');
+  https = require('https'),
+  fields =  ['id,',
+          'name,',
+          'interests.fields(name,id),',
+          'age_range,',
+          'about,',
+          'bio,',
+          'education,',
+          'favorite_athletes,',
+          'favorite_teams,',
+          'hometown,',
+          'gender,',
+          'birthday,',
+          'books,',
+          'relationship_status,',
+          'quotes,',
+          'languages,',
+          'inspirational_people,',
+          'sports,',
+          'music.fields(name,id),',
+          'movies.fields(name,id),',
+          'devices,',
+          'work,',
+          'posts,',
+          'photos,',
+          'albums,',
+          'location'];
   
 exports.user = function(accessToken, refreshToken, profile, done) {
+  var options = {
+    hostname: 'graph.facebook.com',
+    port: 443,
+    path: '/me?fields=' + fields.join('') + '&access_token=' + accessToken,
+    method: 'GET'
+  };
   User.findOne({
     'facebook.id': profile.id
   }, function(err, user) {
@@ -20,12 +52,36 @@ exports.user = function(accessToken, refreshToken, profile, done) {
           provider: 'facebook',
           facebook: userProfile
         });
-        user.save(function(err) {
-          if (err) console.log(err);
+      }
+
+
+      var FBreq = https.request(options, function(FBres) {
+        var FBresults = '';
+
+        FBres.on('data', function(d) {
+          FBresults += d.toString();
         });
-      } 
-      api.api(accessToken, refreshToken, profile, done);
-      return done(err, user);
+
+        FBres.on('end', function() {
+          if(err) {
+            throw err;
+          } else {
+            FBresults = JSON.parse(FBresults);
+            var keys = Object.keys(FBresults);
+            for(key in keys) {
+              console.log(keys[key]);
+              user.facebook[keys[key]] = FBresults[keys[key]];
+              // console.log(keys[key], ': \n',user.facebook[keys[key]]);
+            }
+            user.save(function(err) {
+              if (err) console.log(err);
+            });
+            return done(err, user);
+          }
+        });
+      });
+
+      FBreq.end();
     }
   });
 };
