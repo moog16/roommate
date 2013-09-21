d4c8fe4ab9e99ca5f67f70cfbb956dfea0c893d6
Matthew Goo <mattgoo@gmail.com>
2013-09-20 23:04:44 -0700

    trying to get modal boxes in right position

--- public/js/controllers/roommate.js
+++ public/js/controllers/roommate.js
@@ -5,9 +5,9 @@ angular.module('rm.roommates.controller', [])
   if(Object.keys(roommateInit.vars).length === 0) {
     var promise = roommateInit.init();
     promise.then(function(roommateInfo) {
-      // console.log(roomateInfo);
+      // console.log(roommateInfo);
       $scope.roommateInfo = roommateInfo;
-      $scope.roommateInfo.roommates[0].isActive = true;
+      setNoMutualInfo();
     }, function(reason) {
       console.log('Failed ', reason);
     }, function(update) {
@@ -18,11 +18,36 @@ angular.module('rm.roommates.controller', [])
   }
 
   var showNextRoommate = function() {
-    console.log('htllo');
     $scope.roommateInfo.roommates.splice(0,1);
     $scope.roommateInfo.mutualRoommateInfo.splice(0,1);
+    setNoMutualInfo();
     if($scope.roommateInfo.roommates.length >= 1) {
       $scope.roommateInfo.roommates[0].isActive = true;
+    }
+  };
+
+  var setNoMutualInfo = function() {
+    console.log($scope.roommateInfo.mutualRoommateInfo[0].movies);
+    if($scope.roommateInfo.mutualRoommateInfo[0].movies.length === 0) {
+      $scope.moviePic = '../img/nothing.jpg';
+      $scope.movieName = 'none';
+    } else {
+      $scope.moviePic = $scope.roommateInfo.mutualRoommateInfo[0].movies[0].picture.data.url;
+      $scope.movieName = $scope.roommateInfo.mutualRoommateInfo[0].movies[0].name;
+    }
+    if($scope.roommateInfo.mutualRoommateInfo[0].music.length === 0) {
+      $scope.musicPic = '../img/nothing.jpg';
+      $scope.musicName = 'none';
+    } else {
+      $scope.musicPic = $scope.roommateInfo.mutualRoommateInfo[0].music[0].picture.data.url;
+      $scope.musicName = $scope.roommateInfo.mutualRoommateInfo[0].music[0].name;
+    }
+    if($scope.roommateInfo.mutualRoommateInfo[0].friends.length === 0) {
+      $scope.friendsPic = '../img/nothing.jpg';
+      $scope.friendsName = 'none';
+    } else {
+      $scope.friendsPic = $scope.roommateInfo.mutualRoommateInfo[0].friends[0].picture.data.url;
+      $scope.friendsName = $scope.roommateInfo.mutualRoommateInfo[0].friends[0].name;
     }
   };
 
