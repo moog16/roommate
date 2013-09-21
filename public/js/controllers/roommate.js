71e7bc553e2e6be6cc7a8819c966436f2e49f6c9
Matthew Goo <mattgoo@gmail.com>
2013-09-20 19:36:34 -0700

    made a lot of changes and forgot

--- public/js/controllers/roommate.js
+++ public/js/controllers/roommate.js
@@ -1,20 +1,23 @@
 angular.module('rm.roommates.controller', [])
-.controller('RoommatesController', ['$scope', 'roommateInit',
-  function ($scope, roommateInit) {
+.controller('RoommatesController', ['$scope', '$modal', 'roommateInit',
+  function ($scope, $modal, roommateInit) {
 
-  var promise = roommateInit.init();
-  promise.then(function(roommateInfo) {
-    // console.log(roommateInfo);
-    $scope.roommateInfo = roommateInfo;
-    $scope.roommateInfo.roommates[0].isActive = true;
-  }, function(reason) {
-    console.log('Failed ', reason);
-  }, function(update) {
-    console.log('Got notification ', update);
-  });
+  if(Object.keys(roommateInit.vars).length === 0) {
+    var promise = roommateInit.init();
+    promise.then(function(roommateInfo) {
+      // console.log(roomateInfo);
+      $scope.roommateInfo = roommateInfo;
+      $scope.roommateInfo.roommates[0].isActive = true;
+    }, function(reason) {
+      console.log('Failed ', reason);
+    }, function(update) {
+      console.log('Got notification ', update);
+    });
+  } else {
+    $scope.roommateInfo = roommateInit.vars;
+  }
 
-
-  $scope.showNextRoommate = function() {
+  var showNextRoommate = function() {
     $scope.roommateInfo.roommates.splice(0,1);
     $scope.roommateInfo.mutualRoommateInfo.splice(0,1);
     if($scope.roommateInfo.roommates.length >= 1) {
@@ -22,4 +25,20 @@ angular.module('rm.roommates.controller', [])
     }
   };
 
+  $scope.likeButton = function() {
+    //modal messaging
+    var modalBox = $modal.open({
+      templateUrl: '../public/views/messageModal.html',
+      controller: RoommatesController,
+      resolve: {
+      }
+    })
+    //input into favorites && skipBox
+    //showNextRoommate()
+  };
+
+  $scope.nopeButton = function() {
+    //input into skipBox
+    //showNextRoommate()
+  };
 }]);
