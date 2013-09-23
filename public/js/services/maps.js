c9d39115b5d225c0f148a501971cb281cb4c3adc
Matthew Goo <mattgoo@gmail.com>
2013-09-23 15:57:16 -0700

    cleaned up the autoseach

--- public/js/services/maps.js
+++ public/js/services/maps.js
@@ -1,27 +1,27 @@
 angular.module('rm.maps.service', []).factory("mapsInit", function() {
     var initialize = function() {
-      console.log('maps');
-      var mapOptions = {
-        center: new google.maps.LatLng(-33.8688, 151.2195),
-        zoom: 13,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
 
-      var input = /** @type {HTMLInputElement} */(document.getElementById('searchTextField'));
+      // var mapOptions = {
+      //   center: new google.maps.LatLng(-33.8688, 151.2195),
+      //   zoom: 13,
+      //   mapTypeId: google.maps.MapTypeId.ROADMAP
+      // };
+      // var map = new google.maps.Map(document.getElementById('map-canvas'),
+      //   mapOptions);
+
+      var input = (document.getElementById('searchTextField'));
       var autocomplete = new google.maps.places.Autocomplete(input);
 
-      autocomplete.bindTo('bounds', map);
+      // autocomplete.bindTo('bounds', map);
 
-      var infowindow = new google.maps.InfoWindow();
-      var marker = new google.maps.Marker({
-        map: map
-      });
+      // var infowindow = new google.maps.InfoWindow();
+      // var marker = new google.maps.Marker({
+      //   map: map
+      // });
 
       google.maps.event.addListener(autocomplete, 'place_changed', function() {
-        infowindow.close();
-        marker.setVisible(false);
+        // infowindow.close();
+        // marker.setVisible(false);
         input.className = '';
         var place = autocomplete.getPlace();
         console.log(place);
@@ -31,34 +31,25 @@ angular.module('rm.maps.service', []).factory("mapsInit", function() {
           return;
         }
 
-        // If the place has a geometry, then present it on a map.
-        if (place.geometry.viewport) {
-          map.fitBounds(place.geometry.viewport);
-        } else {
-          map.setCenter(place.geometry.location);
-          map.setZoom(17);  // Why 17? Because it looks good.
-        }
-        marker.setIcon(/** @type {google.maps.Icon} */({
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(35, 35)
-        }));
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);
+        // // If the place has a geometry, then present it on a map.
+        // if (place.geometry.viewport) {
+        //   map.fitBounds(place.geometry.viewport);
+        // } else {
+        //   map.setCenter(place.geometry.location);
+        //   map.setZoom(17);  // Why 17? Because it looks good.
+        // }
+        // marker.setIcon(/* @type {google.maps.Icon}*/ ({
+        //   url: place.icon,
+        //   size: new google.maps.Size(71, 71),
+        //   origin: new google.maps.Point(0, 0),
+        //   anchor: new google.maps.Point(17, 34),
+        //   scaledSize: new google.maps.Size(35, 35)
+        // }));
+        // marker.setPosition(place.geometry.location);
+        // marker.setVisible(true);
 
-        var address = '';
-        if (place.address_components) {
-          address = [
-            (place.address_components[0] && place.address_components[0].short_name || ''),
-            (place.address_components[1] && place.address_components[1].short_name || ''),
-            (place.address_components[2] && place.address_components[2].short_name || '')
-          ].join(' ');
-        }
-
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-        infowindow.open(map, marker);
+        // infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);  //deleted address
+        // infowindow.open(map, marker);
       });
 
     };
